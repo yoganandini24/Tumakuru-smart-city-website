01383dfc940445fca87a5a0ac6af455615feca27
yoganandini24 <129600162+yoganandini24@users.noreply.github.com>
2023-05-29 21:08:09 +0530

    Add files via upload

---
+++ Explore Namma Tumakuru/project/script.js
@@ -0,0 +1,10 @@
+function showPopup() {
+    var popup = document.getElementById("popup");
+    popup.classList.remove("hidden");
+  }
+  
+  function hidePopup() {
+    var popup = document.getElementById("popup");
+    popup.classList.add("hidden");
+  }
+  
